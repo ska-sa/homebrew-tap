67c5b858a0339f76f6a929eb27ddb21094dc9690
Ludwig Schwardt <ludwig.schwardt@gmail.com>
2012-12-11 14:01:10 +0200

    New formula: libair
    Added patches to fix SWIG version checking (for Python binding) and fix a C++ error. The Python binding is still disabled as it needs an automated way to find the NumPy header include path, which would use something like `python -c "import numpy; print numpy.get_include()"`.

---
+++ libair.rb
@@ -0,0 +1,59 @@
+require 'formula'
+
+class Libair < Formula
+  homepage 'http://www.mrao.cam.ac.uk/~bn204/alma/sweng/libairbuild.html'
+  url 'http://www.mrao.cam.ac.uk/~bn204/soft/libair-1.2.tar.bz2'
+  version '1.2'
+  sha1 '32e9c2854f8b5fa8e85fa356e2dc0ba4e1db5859'
+
+  depends_on 'boost'
+  depends_on 'bnmin1'
+  depends_on 'pkg-config' => :build
+
+  def patches
+    # Patch 1: Allow the use of SWIG 2.x for Python bindings
+    # Patch 2: Fix C++ implicit instantiation error
+    DATA
+  end
+
+  def install
+    ENV.deparallelize
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}", "--disable-pybind"
+    system "make install" 
+  end
+end
+
+__END__
+diff --git a/pybind/configure b/pybind/configure
+index 828ea58..0d06394 100755
+--- a/pybind/configure
++++ b/pybind/configure
+@@ -15462,9 +15462,9 @@ $as_echo "$swig_version" >&6; }
+                         if test -z "$available_patch" ; then
+                                 available_patch=0
+                         fi
+-                        if test $available_major -ne $required_major \
+-                                -o $available_minor -ne $required_minor \
+-                                -o $available_patch -lt $required_patch ; then
++                        if test $available_major -lt $required_major \
++                                -o $available_major -eq $required_major -a $available_minor -lt $required_minor \
++                                -o $available_major -eq $required_major -a $available_minor -eq $required_minor -a $available_patch -lt $required_patch ; then
+                                 { $as_echo "$as_me:${as_lineno-$LINENO}: WARNING: SWIG version >= 1.3.31 is required.  You have $swig_version.  You should look at http://www.swig.org" >&5
+ $as_echo "$as_me: WARNING: SWIG version >= 1.3.31 is required.  You have $swig_version.  You should look at http://www.swig.org" >&2;}
+                                 SWIG='echo "Error: SWIG version >= 1.3.31 is required.  You have '"$swig_version"'.  You should look at http://www.swig.org" ; false'
+diff --git a/src/model_water.hpp b/src/model_water.hpp
+index a8d1d3d..fc792c7 100644
+--- a/src/model_water.hpp
++++ b/src/model_water.hpp
+@@ -182,6 +182,9 @@ namespace LibAIR {
+ 
+   }
+ 
++  // Declare explicit specialization before implicit instantiation below
++  template<> const double WaterModel<ICloudyWater>::tau_bump;
++
+   template<>   inline 
+   void WaterModel<ICloudyWater>::dTdTau (std::vector<double> &res) const
+   {
+
