03b97a76c57eaff197d4d15eb3c706cd9d8bd946
Ludwig Schwardt <ludwig.schwardt@gmail.com>
2012-12-11 10:00:47 +0200

    New formula: libwww
    The w3c-libwww library is quite old and not expected to change much. This incorporates the MacPorts patches to make it build on OS X.

---
+++ libwww.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class Libwww < Formula
+  homepage 'http://www.w3.org/Library/'
+  url 'http://www.w3.org/Library/Distribution/w3c-libwww-5.4.0.tgz'
+  version '5.4.0'
+  sha1 '2394cb4e0dc4e2313a9a0ddbf508e4b726e9af63'
+
+  depends_on 'pkg-config' => :build
+
+  def patches
+    # MacPorts patches
+    { :p0 => ['https://trac.macports.org/export/100399/trunk/dports/www/libwww/files/patch-configure.diff',
+              'https://trac.macports.org/export/100399/trunk/dports/www/libwww/files/libwww-config.in.diff']
+    }
+  end
+
+  def install
+    ENV.deparallelize
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+end
