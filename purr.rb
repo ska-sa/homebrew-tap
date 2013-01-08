e369619df2113ec700600b2f5fe08ce65568f47c
Ludwig Schwardt <ludwig.schwardt@gmail.com>
2013-01-08 15:20:29 +0200

    purr: First working version
    Now track the latest release as well as HEAD. Add dependencies on
    pyqt and pyfits. Patch icon loader to look in common MeqTrees share
    directory first. Install Purr icons in this location.

--- purr.rb
+++ purr.rb
@@ -2,11 +2,22 @@ require 'formula'
 
 class Purr < Formula
   homepage 'http://www.astron.nl/meqwiki/Purr/Introduction'
+  url 'https://svn.astron.nl/Purr/release/Purr/release-1.2.0'
   head 'https://svn.astron.nl/Purr/trunk/Purr'
 
-  def install 
+  depends_on 'pyqt'
+  depends_on 'pyfits' => :python
+
+  def patches
+    # First look for icons in meqtrees share directory
+    DATA
+  end
+
+  def install
     mkdir_p "#{lib}/#{which_python}/site-packages"
-    cp_r '.', "#{lib}/#{which_python}/site-packages/"
+    cp_r ['Purr', 'Kittens'], "#{lib}/#{which_python}/site-packages/"
+    mkdir_p "#{share}/meqtrees"
+    cp_r 'icons', "#{share}/meqtrees/"
   end
 
   def caveats; <<-EOS.undent
@@ -27,3 +38,19 @@ class Purr < Formula
     end
   end
 end
+
+__END__
+diff --git a/Kittens/pixmaps.py b/Kittens/pixmaps.py
+index f136a5b..a1f5db4 100644
+--- a/Kittens/pixmaps.py
++++ b/Kittens/pixmaps.py
+@@ -3087,7 +3087,8 @@ class PixmapCache (object):
+     # loop over system path
+     if self._loaded:
+       return;
+-    for path in sys.path:
++    icon_paths = ['/usr/local/share/meqtrees'] + sys.path
++    for path in icon_paths:
+       path = path or '.';
+       # for each entry, try <entry>/icons/<appname>'
+       trydir = os.path.join(path,'icons',self._appname);
