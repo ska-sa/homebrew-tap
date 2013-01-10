b2ef7562c3f4df3d839d3cd0071d9f08bc111279
Ludwig Schwardt <ludwig.schwardt@gmail.com>
2013-01-10 23:58:20 +0200

    casacore 1.5.0
    Update to latest version. Allow parallel compilation so that the
    package installs in a third of the time. Remove patches that are no
    longer required in the new version.

--- casacore.rb
+++ casacore.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Casacore < Formula
   homepage 'http://code.google.com/p/casacore/'
-  url 'http://casacore.googlecode.com/files/casacore-1.4.0.tar.bz2'
-  md5 '85e708b03e73332bbf584310e49a7a2b'
+  url 'http://casacore.googlecode.com/files/casacore-1.5.0.tar.bz2'
+  sha1 'dca7a451c02b141b9e338ba4ffa713693693ce42'
   head 'http://casacore.googlecode.com/svn/trunk'
 
   depends_on 'cmake'
@@ -21,19 +21,12 @@ class Casacore < Formula
   end
 
   def install
-    ENV.j1
     ENV.fortran
     mkdir_p 'build/opt'
     cd 'build/opt'
     system 'cmake', '../..', '-DUSE_FFTW3=ON', "-DFFTW3_ROOT_DIR=#{HOMEBREW_PREFIX}",
            '-DUSE_HDF5=ON', "-DHDF5_ROOT_DIR=#{HOMEBREW_PREFIX}", '-DUSE_THREADS=ON',
            '-DDATA_DIR=/usr/local/share/casacore/data', *std_cmake_args
-    inreplace 'tables/CMakeFiles/casa_tables.dir/build.make', 'RehordGram', 'RecordGram'
-    inreplace 'tables/CMakeFiles/casa_tables.dir/cmake_clean.cmake', 'RehordGram', 'RecordGram'
-    inreplace 'tables/CMakeFiles/casa_tables.dir/DependInfo.cmake', 'RehordGram', 'RecordGram'
-    inreplace 'ms/CMakeFiles/casa_ms.dir/build.make', 'MSShanGram', 'MSScanGram'
-    inreplace 'ms/CMakeFiles/casa_ms.dir/cmake_clean.cmake', 'MSShanGram', 'MSScanGram'
-    inreplace 'ms/CMakeFiles/casa_ms.dir/DependInfo.cmake', 'MSShanGram', 'MSScanGram'
     system "make install"
   end
 end
