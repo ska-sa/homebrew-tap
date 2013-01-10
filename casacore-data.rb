331f5760c30fd17dfa7ee2725ea95d7628a7345f
Ludwig Schwardt <ludwig.schwardt@gmail.com>
2013-01-10 21:22:12 +0200

    New formula: casacore-data
    This provides the meta-data needed by the casacore Measures module
    such as leap second tables, observatory locations and Solar System
    ephemerides. It downloads the latest measures_data tarball from the
    ATNF FTP site (updated weekly). Alternatively it can link to the
    data directory contained in the NRAO CASA application (aka casapy).
    The casacore package looks in /usr/local/share/casacore/data for
    the metadata.

---
+++ casacore-data.rb
@@ -0,0 +1,28 @@
+require 'formula'
+
+class CasacoreData < Formula
+  homepage 'http://code.google.com/p/casacore/'
+  head 'ftp://ftp.atnf.csiro.au/pub/software/measures_data/measures_data.tar.bz2'
+
+  option 'use-casapy', 'Use Mac CASA.App (aka casapy) data directory if found'
+
+  def casapy_data; '/Applications/CASA.app/Contents/data'; end
+
+  def install
+    if (build.include? 'use-casapy') and (File.exists? casapy_data+'/ephemerides')
+      mkdir_p "#{share}/casacore"
+      ln_s "#{casapy_data}", "#{share}/casacore/data"
+    else
+      mkdir_p "#{share}/casacore/data"
+      cp_r Dir['*'], "#{share}/casacore/data/"
+    end
+  end
+
+  def caveats
+    if File.symlink? "#{share}/casacore/data"
+      "Linked to CASA data directory #{casapy_data}"
+    else
+      "Installed latest ATNF measures_data tarball"
+    end
+  end
+end
