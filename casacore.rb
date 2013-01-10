5dcd2acfd0c2107f6ff637900b6bbe802905f22f
Ludwig Schwardt <ludwig.schwardt@gmail.com>
2013-01-10 16:03:31 +0200

    casacore: Fix compiler and refactor
    Add HEAD url. Suppress the latest Lion clang in favour of lvm-gcc.
    Refactor system commands to use FileUtils methods instead. Specify
    the CASA data directory (which will be currently be provided by user).

--- casacore.rb
+++ casacore.rb
@@ -1,9 +1,10 @@
 require 'formula'
 
 class Casacore < Formula
-  url 'http://casacore.googlecode.com/files/casacore-1.4.0.tar.bz2'
   homepage 'http://code.google.com/p/casacore/'
+  url 'http://casacore.googlecode.com/files/casacore-1.4.0.tar.bz2'
   md5 '85e708b03e73332bbf584310e49a7a2b'
+  head 'http://casacore.googlecode.com/svn/trunk'
 
   depends_on 'cmake'
   depends_on 'cfitsio'
@@ -13,7 +14,7 @@ class Casacore < Formula
   depends_on 'readline'
 
   fails_with :clang do
-    build 318
+    build 421
     cause <<-EOS.undent
       CMake reports: Don't know how to enable thread support for /usr/bin/clang
       EOS
@@ -22,15 +23,17 @@ class Casacore < Formula
   def install
     ENV.j1
     ENV.fortran
-    system "mkdir -p build/opt"
-    Dir.chdir("build/opt")
-    system "cmake ../.. -DUSE_HDF5=ON -DUSE_FFTW3=ON -DFFTW3_ROOT_DIR=#{HOMEBREW_PREFIX} -DHDF5_ROOT_DIR=#{HOMEBREW_PREFIX} -DCMAKE_INSTALL_PREFIX=#{prefix} -DUSE_THREADS=ON"
-    system "/usr/bin/sed -i orig -e 's/RehordGram/RecordGram/g' tables/CMakeFiles/casa_tables.dir/build.make"
-    system "/usr/bin/sed -i orig -e 's/RehordGram/RecordGram/g' tables/CMakeFiles/casa_tables.dir/cmake_clean.cmake"
-    system "/usr/bin/sed -i orig -e 's/RehordGram/RecordGram/g' tables/CMakeFiles/casa_tables.dir/DependInfo.cmake"
-    system "/usr/bin/sed -i orig -e 's/MSShanGram/MSScanGram/g' ms/CMakeFiles/casa_ms.dir/build.make"
-    system "/usr/bin/sed -i orig -e 's/MSShanGram/MSScanGram/g' ms/CMakeFiles/casa_ms.dir/cmake_clean.cmake"
-    system "/usr/bin/sed -i orig -e 's/MSShanGram/MSScanGram/g' ms/CMakeFiles/casa_ms.dir/DependInfo.cmake"
+    mkdir_p 'build/opt'
+    cd 'build/opt'
+    system 'cmake', '../..', '-DUSE_FFTW3=ON', "-DFFTW3_ROOT_DIR=#{HOMEBREW_PREFIX}",
+           '-DUSE_HDF5=ON', "-DHDF5_ROOT_DIR=#{HOMEBREW_PREFIX}", '-DUSE_THREADS=ON',
+           '-DDATA_DIR=/usr/local/share/casacore/data', *std_cmake_args
+    inreplace 'tables/CMakeFiles/casa_tables.dir/build.make', 'RehordGram', 'RecordGram'
+    inreplace 'tables/CMakeFiles/casa_tables.dir/cmake_clean.cmake', 'RehordGram', 'RecordGram'
+    inreplace 'tables/CMakeFiles/casa_tables.dir/DependInfo.cmake', 'RehordGram', 'RecordGram'
+    inreplace 'ms/CMakeFiles/casa_ms.dir/build.make', 'MSShanGram', 'MSScanGram'
+    inreplace 'ms/CMakeFiles/casa_ms.dir/cmake_clean.cmake', 'MSShanGram', 'MSScanGram'
+    inreplace 'ms/CMakeFiles/casa_ms.dir/DependInfo.cmake', 'MSShanGram', 'MSScanGram'
     system "make install"
   end
 end
