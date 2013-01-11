f7a8c2d49842bc15458fcd320c27f130cb75c23c
Ludwig Schwardt <ludwig.schwardt@gmail.com>
2013-01-11 17:35:33 +0200

    New formula: cattery
    This package is part of the MeqTrees suite and provides a bunch of
    high-level Python modules. It is quite tightly coupled to Timba aka
    meqtrees but given a separate formula as it resides in a separate
    part of the MeqTrees repository with its own release cycle. Since
    the installation part is trivial this then becomes a dependency for
    the meqtrees package.

---
+++ cattery.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+class Cattery < Formula
+  homepage 'http://www.astron.nl/meqwiki/MeqTrees'
+  url 'https://svn.astron.nl/MeqTrees/release/Cattery/release-1.2.0'
+  head 'https://svn.astron.nl/MeqTrees/trunk/Cattery'
+
+  def install
+    mkdir_p "#{lib}/#{which_python}/site-packages"
+    rm_f 'Meow/LSM0'
+    cp_r 'LSM', 'Meow/LSM0'
+    cp_r ['Calico', 'LSM', 'Lions', 'Meow', 'Scripter', 'Siamese', 'qt.py'],
+         "#{lib}/#{which_python}/site-packages/"
+    mkdir_p "#{share}/meqtrees"
+    cp_r 'test', "#{share}/meqtrees/"
+  end
+
+  def caveats; <<-EOS.undent
+    For non-homebrew Python, you need to amend your PYTHONPATH like so:
+      export PYTHONPATH=#{HOMEBREW_PREFIX}/lib/#{which_python}/site-packages:$PYTHONPATH
+    EOS
+  end
+
+  def which_python
+    "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
+  end
+end
