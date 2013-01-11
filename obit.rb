65b83698ce272456142dfb716bef7cd14b26cdf1
Ludwig Schwardt <ludwig.schwardt@gmail.com>
2013-01-11 11:07:50 +0200

    obit: Moved test to end of class

--- obit.rb
+++ obit.rb
@@ -99,6 +99,16 @@ class Obit < Formula
     system 'make', 'install', "prefix=#{prefix}"
   end
 
+  def caveats; <<-EOS.undent
+    For non-homebrew Python, you need to amend your PYTHONPATH like so:
+      export PYTHONPATH=#{HOMEBREW_PREFIX}/lib/#{which_python}/site-packages:$PYTHONPATH
+    EOS
+  end
+
+  def which_python
+    "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
+  end
+
   def test
     mktemp do
       # Test plotting functionality via pgplot / plplot
@@ -110,16 +120,6 @@ class Obit < Formula
         onoe 'testContourPlot FAILED'
       end
     end
-  end
-
-  def caveats; <<-EOS.undent
-    For non-homebrew Python, you need to amend your PYTHONPATH like so:
-      export PYTHONPATH=#{HOMEBREW_PREFIX}/lib/#{which_python}/site-packages:$PYTHONPATH
-    EOS
-  end
-
-  def which_python
-    "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
   end
 end
 
