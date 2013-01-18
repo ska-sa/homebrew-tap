896766260e9bb67f1c1658152a1919b86596b296
Ludwig Schwardt <ludwig.schwardt@gmail.com>
2013-01-19 01:01:44 +0200

    meqtrees: Fix patches for 1.2.1
    Fix patches to work both with HEAD and version 1.2.1. This required
    moving them from DATA to gist URLs to allow cherry-picking them.
    Add a patch needed to build version 1.2.1. This still has a problem
    with meqbrowser not connecting to meqserver via Unix sockets.

--- meqtrees.rb
+++ meqtrees.rb
@@ -34,12 +34,20 @@ class Meqtrees < Formula
   depends_on 'makems'
 
   def patches
+    p = []
     # Added explicit template instantiation and corrected constness
+    p << 'https://gist.github.com/raw/4568292/e187cef9d60d4b89293f2d6b93ad9940ccd9c5aa/patch1.diff'
     # Use correct version of strerror_r on the Mac
+    p << 'https://gist.github.com/raw/4568292/21ebb9fd3094b18c37555ef1288915036c623c03/patch2.diff'
     # Fixed bug in thread map index
+    p << 'https://gist.github.com/raw/4568292/058d009cd7e90f9578d90494b508b96968dddd13/patch3.diff'
     # Add support for Blitz++ 0.10
+    p << 'https://gist.github.com/raw/4568292/76627df1f718eceef29fa3e224d2bfee90c3ce06/patch4.diff'
+    # Disambiguate Mutex::Lock class (fixed in HEAD)
+    p << 'https://gist.github.com/raw/4568292/38983609880a37d0a93d626628d31fbba76accd2/patch5.diff' if not build.head?
     # Suppress compiler warning by using correct format specifier
-    DATA
+    p << 'https://gist.github.com/raw/4568292/f978679da33f843a6260d4c7a36f4c021174d32c/patch6.diff' if build.head?
+    return p.empty? ? nil : p
   end
 
   fails_with :clang do
@@ -153,195 +161,3 @@ class Meqtrees < Formula
     system "false"
   end
 end
-
-__END__
-diff --git a/DMI/src/DMI.h b/DMI/src/DMI.h
-index 49e2e9b..8917bcf 100644
---- a/DMI/src/DMI.h
-+++ b/DMI/src/DMI.h
-@@ -110,6 +110,7 @@ namespace DMI
-   // compile-time error reporting. This is borrowed from Alexandrescu
-   template<int> struct CompileTimeError;
-   template<> struct CompileTimeError<true> {};
-+  template<> struct CompileTimeError<false> {};
- 
- };
- 
-diff --git a/DMI/src/Packer.h b/DMI/src/Packer.h
-index 2ef85ae..52372d5 100644
---- a/DMI/src/Packer.h
-+++ b/DMI/src/Packer.h
-@@ -525,8 +525,8 @@ template <class Map, class KeyPacker, class ValuePacker>
- void MapPacker<Map,KeyPacker,ValuePacker>::unpack (Map &mp, const void *block, size_t sz)
- {
-   FailWhen(sz<sizeof(size_t),"corrupt block");
--  const size_t 
--    *hdr = static_cast<size_t*>(block),
-+  size_t 
-+    *hdr = const_cast<size_t*>(static_cast<const size_t*>(block)),
-     n   = *(hdr++),
-     sz0 = (1+ (KeyPacker::binary()?0:n) + (ValuePacker::binary()?0:n))*sizeof(size_t);
-   FailWhen(sz<sz0,"corrupt block");
-diff --git a/MEQ/src/FastParmTable.cc b/MEQ/src/FastParmTable.cc
-index c0e53fc..7034607 100644
---- a/MEQ/src/FastParmTable.cc
-+++ b/MEQ/src/FastParmTable.cc
-@@ -164,7 +164,15 @@ void FastParmTable::throwErrno (const string &message)
- {
-   int errno0 = errno;
-   char errbuf[256];
-+// This test was taken from Chromium's safe_strerror_posix.cc
-+#if (defined(__GLIBC__) || defined(OS_NACL))
-+  // Use the historical GNU-specific version of strerror_r found on e.g. Linux
-   char *err = strerror_r(errno0,errbuf,sizeof(errbuf));
-+#else
-+  // Use the POSIX-compliant (XSI-compliant) version of strerror_r found on e.g. Darwin
-+  int errno1 = strerror_r(errno0,errbuf,sizeof(errbuf));
-+  char *err = errbuf;
-+#endif
-   Throw(Debug::ssprintf("%s: %s (errno=%d)",
-         Debug::ssprintf(message.c_str(),table_name_.c_str()).c_str(),err,errno0));
- }
-diff --git a/TimBase/src/Thread/Thread.cc b/TimBase/src/Thread/Thread.cc
-index c90c7af..b673098 100644
---- a/TimBase/src/Thread/Thread.cc
-+++ b/TimBase/src/Thread/Thread.cc
-@@ -74,7 +74,7 @@ namespace LOFAR
-       pthread_t id = 0;
-       pthread_create(&id,attr,start,arg);
-       // add to map
--      thread_map_[thread_list_.size()] = id;
-+      thread_map_[id] = thread_list_.size();
-       thread_list_.push_back(id);
-       return id;
-     }
-diff --git a/MEQ/src/ComposedPolc.cc b/MEQ/src/ComposedPolc.cc
-index 510b158..e895ef6 100644
---- a/MEQ/src/ComposedPolc.cc
-+++ b/MEQ/src/ComposedPolc.cc
-@@ -266,13 +266,13 @@ void ComposedPolc::validateContent (bool recursive)
- 	  starti[axisi]=0;endi[axisi]=0;continue;
- 	}
- 	if (!polcdom.isDefined(axisi)){
--	  starti[axisi]=0;endi[axisi]=std::min(res_shape[axisi]-1,startgrid[axisi].size()-1);continue;
-+	  starti[axisi]=0;endi[axisi]=std::min(res_shape[axisi]-1,static_cast<int>(startgrid[axisi].size())-1);continue;
- 	}
--	int maxk=std::min(res_shape[axisi],startgrid[axisi].size());
-+	int maxk=std::min(res_shape[axisi],static_cast<int>(startgrid[axisi].size()));
- 	int k=0;
- 	while(k<maxk  && centergrid[axisi](k)<polcdom.start(axisi)) k++;
- 	starti[axisi] = k;
--	k=std::min(res_shape[axisi]-1,startgrid[axisi].size()-1);
-+	k=std::min(res_shape[axisi]-1,static_cast<int>(startgrid[axisi].size())-1);
- 	while(k>0 && (centergrid[axisi](k)>polcdom.end(axisi))) k--;
- 	endi[axisi] = k;
- 	cdebug(3)<<"axis : "<<axisi<<" begin : "<<starti[axisi]<<" end : "<<endi[axisi]<<endl;
-diff --git a/MEQ/src/Polc.cc b/MEQ/src/Polc.cc
-index 130f0e0..da0b1f8 100644
---- a/MEQ/src/Polc.cc
-+++ b/MEQ/src/Polc.cc
-@@ -224,7 +224,7 @@ void Polc::do_evaluate (VellSet &vs,const Cells &cells,
-       grid[i] = ( grid[i] - getOffset(i) )*one_over_scale;
- 
-       cdebug(4)<<"calculating polc on grid "<<i<<" : "<<grid[i]<<endl;
--      res_shape[iaxis] = std::max(grid[i].size(),1);
-+      res_shape[iaxis] = std::max(static_cast<int>(grid[i].size()),1);
-     }
-   }
-   // now evaluate
-diff --git a/MEQ/src/Spline.cc b/MEQ/src/Spline.cc
-index beaccaa..ae001fc 100644
---- a/MEQ/src/Spline.cc
-+++ b/MEQ/src/Spline.cc
-@@ -195,7 +195,7 @@ void Spline::do_evaluate (VellSet &vs,const Cells &cells,
-             " is not defined in Cells");
-       grid[i].resize(cells.ncells(iaxis));
-       grid[i] = cells.center(iaxis);
--      res_shape[iaxis] = std::max(grid[i].size(),1);
-+      res_shape[iaxis] = std::max(static_cast<int>(grid[i].size()),1);
-       total*=res_shape[iaxis];
-     }
-   }
-diff --git a/TimBase/src/Lorrays-Blitz.h b/TimBase/src/Lorrays-Blitz.h
-index d31b03d..11ca33f 100644
---- a/TimBase/src/Lorrays-Blitz.h
-+++ b/TimBase/src/Lorrays-Blitz.h
-@@ -258,16 +258,29 @@ class VariVector : public std::vector<int>
-       VariVector (int n1,int n2,int n3,int n4,int n5) : std::vector<int>(5)
-         { iterator iter = begin(); *iter++=n1; *iter++=n2; *iter++=n3; *iter++=n4; *iter++=n5; }
-       
--      // construct from TinyVector
-+      // construct from TinyVector<int,N>
-       // (this assumes contiguity in TinyVector, which is probably pretty safe to assume)
-       template<int N>
-       VariVector( const blitz::TinyVector<int,N> &tvec )
-           : std::vector<int>(tvec.data(),tvec.data() + N) {};
--      // convert to TinyVector
-+      // convert to TinyVector<int,N>
-       template<int N>
-       operator blitz::TinyVector<int,N> () const
-       { 
--        blitz::TinyVector<int,N> tvec(0);
-+        int initial_value = 0;
-+        blitz::TinyVector<int,N> tvec(initial_value);
-+        for( int i = 0; i < std::min(N,(int)size()); i++ )
-+          tvec[i] = (*this)[i];
-+        return tvec;
-+      }
-+      // convert to TinyVector<long int,N>
-+      // Since Blitz++ 0.10 the Blitz::Array stride has a different type (diffType) to the shape (int)
-+      // and on at least some platforms diffType is long int
-+      template<int N>
-+      operator blitz::TinyVector<long int,N> () const
-+      { 
-+        long int initial_value = 0;
-+        blitz::TinyVector<long int,N> tvec(initial_value);
-         for( int i = 0; i < std::min(N,(int)size()); i++ )
-           tvec[i] = (*this)[i];
-         return tvec;
-diff --git a/MeqNodes/src/TFSmearFactorApprox.cc b/MeqNodes/src/TFSmearFactorApprox.cc
-index fb25bdf..4d10f54 100644
---- a/MeqNodes/src/TFSmearFactorApprox.cc
-+++ b/MeqNodes/src/TFSmearFactorApprox.cc
-@@ -54,22 +54,22 @@ using namespace VellsMath;
- // for teh normal stencils: used to have
- //    A = .5*central12(B,blitz::firstDim);
- BZ_DECLARE_STENCIL2(TimeDiff, A,B)
--    A = forward11(B,blitz::firstDim);
-+    A = forward11_stencilop(B,blitz::firstDim);
- BZ_END_STENCIL
- BZ_DECLARE_STENCIL2(TimeDiff1,A,B)
--    A = forward11(B,blitz::firstDim);
-+    A = forward11_stencilop(B,blitz::firstDim);
- BZ_END_STENCIL
- BZ_DECLARE_STENCIL2(TimeDiff2,A,B)
--    A = backward11(B,blitz::firstDim);
-+    A = backward11_stencilop(B,blitz::firstDim);
- BZ_END_STENCIL
- BZ_DECLARE_STENCIL2(FreqDiff, A,B)
--    A = forward11(B,blitz::secondDim);
-+    A = forward11_stencilop(B,blitz::secondDim);
- BZ_END_STENCIL
- BZ_DECLARE_STENCIL2(FreqDiff1,A,B)
--    A = forward11(B,blitz::secondDim);
-+    A = forward11_stencilop(B,blitz::secondDim);
- BZ_END_STENCIL
- BZ_DECLARE_STENCIL2(FreqDiff2,A,B)
--    A = backward11(B,blitz::secondDim);
-+    A = backward11_stencilop(B,blitz::secondDim);
- BZ_END_STENCIL
- 
- 
-diff --git a/MeqNodes/src/CUDAPointSourceVisibility.cc b/MeqNodes/src/CUDAPointSourceVisibility.cc
-index 3dec9d0..fe8a4f5 100644
---- a/MeqNodes/src/CUDAPointSourceVisibility.cc
-+++ b/MeqNodes/src/CUDAPointSourceVisibility.cc
-@@ -151,7 +151,7 @@ void CUDAPointSourceVisibility::checkTensorDims (int ichild,const LoShape &shape
-   else
-   {
-     n = shape[0];
--    printf("child %i is dim %i\n", ichild, shape.size());
-+    printf("child %i is dim %zu\n", ichild, shape.size());
-     if( shape.size() == 2 )
-     {
-         printf("                 %i x %i\n", shape[0], shape[1]);
