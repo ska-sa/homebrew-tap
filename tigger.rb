bf19c2fc71f15ffe65b9636fca02df4e3f8d37f0
Ludwig Schwardt <ludwig.schwardt@gmail.com>
2013-01-19 00:56:33 +0200

    tigger 1.2.2
    Fix patches to work both with HEAD and the last release (1.2.2) by
    leaving out part of patch 2 for a non-existent file in version 1.2.2.
    This required moving the patches from DATA to gist URLs to be able to
    cherry-pick them.

--- tigger.rb
+++ tigger.rb
@@ -13,9 +13,15 @@ class Tigger < Formula
   depends_on 'astLib' => :python
 
   def patches
+    p = []
     # Ignore setAllowX11ColorNames attribute on non-X11 Mac platform
+    p << 'https://gist.github.com/raw/4565060/3a11913eff383b188a7dc887d560140b2b2b3500/patch1.diff'
     # Fix imports to be absolute
-    DATA
+    p << 'https://gist.github.com/raw/4565060/b42096bf9569b60a11f99a7432826e6235be5c4b/patch2.diff'
+    p << 'https://gist.github.com/raw/4565060/2bc9f42182180401adb52b8be065d1c70d39fe37/patch3.diff' if build.head?
+    # Nuke matplotlib differently (dummy_module clashes with pkg_resources)
+    p << 'https://gist.github.com/raw/4565060/95dc15b776855ed4273c79d18e1c99177ea8b41a/patch4.diff'
+    return p.empty? ? nil : p
   end
 
   def install
@@ -48,141 +54,3 @@ class Tigger < Formula
     end
   end
 end
-
-__END__
-diff --git a/Models/PlotStyles.py b/Models/PlotStyles.py
-index 19cdf96..2cda4a7 100644
---- a/Models/PlotStyles.py
-+++ b/Models/PlotStyles.py
-@@ -45,7 +45,11 @@ StyleAttributeTypes = dict(symbol_size=int,symbol_linewidth=int,label_size=int);
- # list of known colors
- ColorList = [ "black","blue","lightblue","green","lightgreen","cyan","red","orange red","purple","magenta","yellow","white" ];
- DefaultColor = "black";
--QColor.setAllowX11ColorNames(True);
-+# Ignore this (non-existent) attribute on non-X11 platforms like the Mac
-+try:
-+    QColor.setAllowX11ColorNames(True);
-+except AttributeError:
-+    pass
- 
- # dict and method to pick a contrasting color (i.e. suitable as background for specified color)
- ContrastColor = dict(white="#404040",yellow="#404040");
-diff --git a/Images/ControlDialog.py b/Images/ControlDialog.py
-index 856b7c5..0f5e0b6 100644
---- a/Images/ControlDialog.py
-+++ b/Images/ControlDialog.py
-@@ -37,7 +37,7 @@ import Kittens.utils
- from Kittens.utils import curry,PersistentCurrier
- from Kittens.widgets import BusyIndicator
- 
--from Images import SkyImage,Colormaps
-+from Tigger.Images import SkyImage,Colormaps
- from Tigger import pixmaps
- from Tigger.Widgets import FloatValidator,TiggerPlotCurve,TiggerPlotMarker
- 
-diff --git a/Images/Controller.py b/Images/Controller.py
-index 1af776e..ea1b46c 100644
---- a/Images/Controller.py
-+++ b/Images/Controller.py
-@@ -42,15 +42,15 @@ _verbosity = Kittens.utils.verbosity(name="imagectl");
- dprint = _verbosity.dprint;
- dprintf = _verbosity.dprintf;
- 
--from Images import SkyImage,Colormaps
--from Models import ModelClasses,PlotStyles
--from Coordinates import Projection,radec_string;
--from Models.SkyModel import SkyModel
-+from Tigger.Images import SkyImage,Colormaps
-+from Tigger.Models import ModelClasses,PlotStyles
-+from Tigger.Coordinates import Projection,radec_string;
-+from Tigger.Models.SkyModel import SkyModel
- from Tigger import pixmaps
- from Tigger.Widgets import FloatValidator
- 
--from RenderControl import RenderControl
--from ControlDialog import ImageControlDialog
-+from Tigger.Images.RenderControl import RenderControl
-+from Tigger.Images.ControlDialog import ImageControlDialog
- 
- class ImageController (QFrame):
-   """An ImageController is a widget for controlling the display of one image.
-diff --git a/Images/Manager.py b/Images/Manager.py
-index 0168c5a..240bfd3 100644
---- a/Images/Manager.py
-+++ b/Images/Manager.py
-@@ -38,9 +38,9 @@ import Kittens.utils
- from Kittens.utils import curry,PersistentCurrier
- from Kittens.widgets import BusyIndicator
- 
--from Controller import ImageController,dprint,dprintf
-+from Tigger.Images.Controller import ImageController,dprint,dprintf
- 
--import SkyImage
-+from Tigger.Images import SkyImage
- from Tigger.Images import  FITS_ExtensionList
- 
- class ImageManager (QWidget):
-diff --git a/Images/RenderControl.py b/Images/RenderControl.py
-index def7906..5ccab51 100644
---- a/Images/RenderControl.py
-+++ b/Images/RenderControl.py
-@@ -41,7 +41,7 @@ _verbosity = Kittens.utils.verbosity(name="rc");
- dprint = _verbosity.dprint;
- dprintf = _verbosity.dprintf;
- 
--from Images import SkyImage,Colormaps
-+from Tigger.Images import SkyImage,Colormaps
- from Tigger import pixmaps,ConfigFile
- from Tigger.Widgets import FloatValidator
- 
-diff --git a/Images/SkyImage.py b/Images/SkyImage.py
-index 889896c..f2c9fed 100644
---- a/Images/SkyImage.py
-+++ b/Images/SkyImage.py
-@@ -40,8 +40,8 @@ import Kittens.utils
- pyfits = Kittens.utils.import_pyfits();
- 
- from Tigger.Coordinates import Projection
--import Colormaps
--import FITSHeaders
-+from Tigger.Images import Colormaps
-+from Tigger.Images import FITSHeaders
- 
- DEG = math.pi/180;
- 
-diff --git a/SkyModelTreeWidget.py b/SkyModelTreeWidget.py
-index 0fc57e3..d0800f4 100644
---- a/SkyModelTreeWidget.py
-+++ b/SkyModelTreeWidget.py
-@@ -32,8 +32,8 @@ import Kittens.utils
- from Kittens.utils import PersistentCurrier
- from Kittens.widgets import BusyIndicator
- 
--from Models import ModelClasses,PlotStyles
--from Models.SkyModel import SkyModel
-+from Tigger.Models import ModelClasses,PlotStyles
-+from Tigger.Models.SkyModel import SkyModel
- 
- _verbosity = Kittens.utils.verbosity(name="tw");
- dprint = _verbosity.dprint;
-diff --git a/Plot/SkyModelPlot.py b/Plot/SkyModelPlot.py
-index 4654614..7ce79f9 100644
---- a/Plot/SkyModelPlot.py
-+++ b/Plot/SkyModelPlot.py
-@@ -43,12 +43,11 @@ dprintf = _verbosity.dprintf;
- 
- from Tigger import pixmaps,Config,ConfigFile
- from Tigger.Models import ModelClasses,PlotStyles
--import Coordinates
--from Coordinates import Projection
--from  Models.SkyModel import SkyModel
--import MainWindow
-+from Tigger import Coordinates
-+from Tigger.Coordinates import Projection
-+from Tigger.Models.SkyModel import SkyModel
- from Tigger.Widgets import TiggerPlotCurve,TiggerPlotMarker
--import MouseModes
-+from Tigger.Plot import MouseModes
- 
- # plot Z depths for various classes of objects
- Z_Image = 1000;
