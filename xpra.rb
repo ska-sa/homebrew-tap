8dc8ca2d77b5624a9b935190d59f41e209717804
Ludwig Schwardt <ludwig@ska.ac.za>
2013-11-22 12:59:33 +0200

    xpra 0.10.9

--- xpra.rb
+++ xpra.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Xpra < Formula
   homepage 'http://xpra.org'
-  url 'https://www.xpra.org/src/xpra-0.7.8.tar.bz2'
-  sha1 '0f8b433c97b707e555c4b45e6a411c007baebdf8'
+  url 'http://winswitch.org/src/xpra-0.10.9.tar.bz2'
+  sha1 '1fe8113242143d51f492502e1d0c7441c34cbf23'
 
   # We want pkg-config
   env :userpaths
@@ -16,11 +16,6 @@ class Xpra < Formula
   depends_on 'webp'
   depends_on 'Cython' => :python
 
-  def patches
-    # 1. Fix pygtk include directory
-    DATA
-  end
-
   def install
     python do
       system python, "setup.py", "install", "--prefix=#{prefix}"
@@ -31,18 +26,3 @@ class Xpra < Formula
     python.standard_caveats if python
   end
 end
-
-__END__
-diff --git a/wimpiggy/gdk/gdk_atoms.pyx b/wimpiggy/gdk/gdk_atoms.pyx
-index 900aa66..0a1392f 100644
---- a/wimpiggy/gdk/gdk_atoms.pyx
-+++ b/wimpiggy/gdk/gdk_atoms.pyx
-@@ -23,7 +23,7 @@ cdef extern from "pygobject.h":
-     void init_pygobject()
- init_pygobject()
- 
--cdef extern from "pygtk/pygtk.h":
-+cdef extern from "pygtk-2.0/pygtk/pygtk.h":
-     void init_pygtk()
- init_pygtk()
- # Now all the macros in those header files will work.
