10421b09d94acf21cb6c5871d613c77f51ec0193
Ludwig Schwardt <ludwig@ska.ac.za>
2014-03-21 00:39:35 +0200

    casarest: Disable clang and patches for now
    The latest clang still does not like the GCC extensions in use (e.g.
    variable-length arrays). This will need some actual code fixes as
    everyone should use clang on Homebrew.
    
    Hard-code libstdc++ to avoid compiler clashes on clang. Ignore the
    patches for now as they seem to be obsolete on the stable version
    as well (?).

--- casarest.rb
+++ casarest.rb
@@ -14,25 +14,27 @@ class Casarest < Formula
   depends_on :fortran
 
   fails_with :clang do
-    build 425
+    build 503
     cause <<-EOS.undent
       Code does not follow C++ standard strictly but does whatever GCC allows
       EOS
   end
 
-  def patches
-    p = []
-    # Fixes disallowed size_t vs int* comparison, which used to be specially
-    # included for Darwin systems, but does not seem relevant anymore (fixed in HEAD).
-    p << 'https://gist.github.com/raw/4705907/678753a3fc04751457271c82f4a3fe39149b5819/patch1.diff' if not build.head?
-    # Add boost_system library to avoid missing symbols (fixed in HEAD)
-    p << 'https://gist.github.com/raw/4705907/a199623d33e3dd566a8ffe15cc0448f6e771e44d/patch2.diff' if not build.head?
-    return p.empty? ? nil : p
-  end
+  # def patches
+  #   p = []
+  #   # Fixes disallowed size_t vs int* comparison, which used to be specially
+  #   # included for Darwin systems, but does not seem relevant anymore (fixed in HEAD).
+  #   p << 'https://gist.github.com/raw/4705907/678753a3fc04751457271c82f4a3fe39149b5819/patch1.diff' if not build.head?
+  #   # Add boost_system library to avoid missing symbols (fixed in HEAD)
+  #   p << 'https://gist.github.com/raw/4705907/a199623d33e3dd566a8ffe15cc0448f6e771e44d/patch2.diff' if not build.head?
+  #   return p.empty? ? nil : p
+  # end
 
   def install
     # Workaround to get fortran and C++ to play together (see Homebrew issue #20173)
     ENV.append 'LDFLAGS', "-L/usr/lib -lstdc++"
+    # Force clang to use the old standard library for now (solves issue with complex type)
+    ENV.append 'CXXFLAGS', "-stdlib=libstdc++" if ENV.compiler == :clang
     mkdir_p 'build'
     cd 'build'
     cmake_args = std_cmake_args
