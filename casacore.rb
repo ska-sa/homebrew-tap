174c7d79582b70557502c9a56df644bb1fc5600e
Ludwig Schwardt <ludwig@ska.ac.za>
2015-03-27 10:48:38 +0200

    casacore: Enable devel version that works
    The Nov14 branch is actually the new master branch that will soon
    be released as casacore 2.0.0. It contains compilation fixes for clang.
    These errors only used to manifest itself within pyrap, being templates.
    Add a small patch that fixes a bug introduced 2 days ago - this will
    hopefully go into upstream soon.

--- casacore.rb
+++ casacore.rb
@@ -1,10 +1,22 @@
 require 'formula'
 
+class CasacoreDownloadStrategy < SubversionDownloadStrategy
+  def stage
+    mkdir_p "casacore"
+    cd "casacore"
+    super
+  end
+end
+
 class Casacore < Formula
   homepage 'http://code.google.com/p/casacore/'
   url 'ftp://ftp.atnf.csiro.au/pub/software/casacore/casacore-1.7.0.tar.bz2'
   sha1 '03edc1c8b8c3fbee91df4f9874f5db7b9d403035'
   head 'http://casacore.googlecode.com/svn/trunk'
+  devel do
+    url 'http://casacore.googlecode.com/svn/branches/nov14/', :using => CasacoreDownloadStrategy
+    version "2.0.0dev"
+  end
 
   depends_on 'cmake' => :build
   depends_on 'cfitsio'
@@ -14,6 +26,11 @@ class Casacore < Formula
   depends_on 'readline'
   depends_on 'casacore-data'
   depends_on :fortran
+
+  devel do
+    # Fix install bug
+    patch :DATA
+  end
 
   def install
     # To get a build type besides "release" we need to change from superenv to std env first
@@ -37,3 +54,29 @@ class Casacore < Formula
     end
   end
 end
+
+__END__
+diff --git a/ms/CMakeLists.txt b/ms/CMakeLists.txt
+index 260095d..e7000e6 100644
+--- a/ms/CMakeLists.txt
++++ b/ms/CMakeLists.txt
+@@ -195,7 +195,6 @@ MeasurementSets/MSMainColumns.h
+ MeasurementSets/MSMainEnums.h
+ MeasurementSets/MSObsColumns.h
+ MeasurementSets/MSObsEnums.h
+-MeasurementSets/MSObsIndex.h
+ MeasurementSets/MSObservation.h
+ MeasurementSets/MSPointing.h
+ MeasurementSets/MSPointingColumns.h
+@@ -223,11 +222,9 @@ MeasurementSets/MSTable.h
+ MeasurementSets/MSTable.tcc
+ MeasurementSets/MSTableImpl.h
+ MeasurementSets/MSTileLayout.h
+-MeasurementSets/MSTimeDefinitions.h
+ MeasurementSets/MSWeather.h
+ MeasurementSets/MSWeatherColumns.h
+ MeasurementSets/MSWeatherEnums.h
+-MeasurementSets/MSWeatherIndex.h
+ MeasurementSets/MeasurementSet.h
+ MeasurementSets/StokesConverter.h
+ DESTINATION include/casacore/ms/MeasurementSets
