0939d0e5efb90809a7ccf4a7d3f21c4e6320e82a
Ludwig Schwardt <ludwig@ska.ac.za>
2015-05-04 15:11:25 +0200

    casacore 2.0.1
    This now pulls from the new GitHub home of casacore. The hacky download
    strategy is not needed anymore, as is the devel block for trying out
    the 2.0 branch.
    
    There is no requirement to use C++11, as the Complex type has been fixed.
    Python support is included by default, though, as pyrap and python-casacore
    won't work otherwise. Both these are turned into options.
    
    Fix the tests to be more brewy. Pass the strict audit.

--- casacore.rb
+++ casacore.rb
@@ -1,51 +1,40 @@
-require 'formula'
-
-class CasacoreDownloadStrategy < SubversionDownloadStrategy
-  def stage
-    mkdir_p "casacore"
-    cd "casacore"
-    super
-  end
-end
-
 class Casacore < Formula
-  homepage 'http://code.google.com/p/casacore/'
-  url 'ftp://ftp.atnf.csiro.au/pub/software/casacore/casacore-1.7.0.tar.bz2'
-  sha1 '03edc1c8b8c3fbee91df4f9874f5db7b9d403035'
-  head 'http://casacore.googlecode.com/svn/trunk'
-  devel do
-    url 'http://casacore.googlecode.com/svn/branches/nov14/', :using => CasacoreDownloadStrategy
-    version "2.0.0dev"
-  end
+  homepage "https://github.com/casacore/casacore/wiki"
+  url "https://github.com/casacore/casacore/archive/v2.0.1.tar.gz"
+  sha256 "ed43412d9250f702b5a5d5973ea30bc51047d9e6adc2f801eb2fcd6274de2201"
+  head "https://github.com/casacore/casacore.git"
 
-  depends_on 'cmake' => :build
-  depends_on 'cfitsio'
-  depends_on 'wcslib'
-  depends_on 'fftw'
-  depends_on 'hdf5'
-  depends_on 'readline'
-  depends_on 'casacore-data'
+  option "without-python", "Don't build with Python support (pyrap and python-casacore won't work though)"
+  option "with-cxx11", "Build with C++11 support"
+
+  depends_on "cmake" => :build
+  depends_on "cfitsio"
+  depends_on "wcslib"
+  depends_on "fftw"
+  depends_on "hdf5"
+  depends_on "readline"
+  depends_on "casacore-data"
   depends_on :fortran
 
   def install
     # To get a build type besides "release" we need to change from superenv to std env first
-    build_type = 'release'
+    build_type = "release"
     mkdir_p "build/#{build_type}"
     cd "build/#{build_type}"
     cmake_args = std_cmake_args
-    cmake_args.delete '-DCMAKE_BUILD_TYPE=None'
+    cmake_args.delete "-DCMAKE_BUILD_TYPE=None"
     cmake_args << "-DCMAKE_BUILD_TYPE=#{build_type}"
-    cmake_args << "-DCXX11=ON" if ENV.compiler == :clang
-    cmake_args << '-DUSE_FFTW3=ON' << "-DFFTW3_ROOT_DIR=#{HOMEBREW_PREFIX}"
-    cmake_args << '-DUSE_HDF5=ON' << "-DHDF5_ROOT_DIR=#{HOMEBREW_PREFIX}"
-    cmake_args << '-DUSE_THREADS=ON' << "-DDATA_DIR=#{HOMEBREW_PREFIX}/share/casacore/data"
-    system 'cmake', '../..', *cmake_args
-    system "make install"
+    cmake_args << "-DCXX11=ON" if build.with? "cxx11"
+    cmake_args << "-DBUILD_PYTHON=ON" if build.with? "python"
+    cmake_args << "-DUSE_FFTW3=ON" << "-DFFTW3_ROOT_DIR=#{HOMEBREW_PREFIX}"
+    cmake_args << "-DUSE_HDF5=ON" << "-DHDF5_ROOT_DIR=#{HOMEBREW_PREFIX}"
+    cmake_args << "-DUSE_THREADS=ON" << "-DDATA_DIR=#{HOMEBREW_PREFIX}/share/casacore/data"
+    system "cmake", "../..", *cmake_args
+    system "make", "install"
   end
 
-  def test
-    if not system 'findmeastable IGRF' and not system 'findmeastable DE405'
-      ohai 'casacore OK'
-    end
+  test do
+    system bin/"findmeastable", "IGRF"
+    system bin/"findmeastable", "DE405"
   end
 end
