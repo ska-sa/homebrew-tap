05808e8a43be4b93d9a50b691392c889ba11f2d0
Ludwig Schwardt <ludwig@ska.ac.za>
2018-06-05 23:44:55 +0200

    Fix text message (double 'because')

--- obit.rb
+++ obit.rb
@@ -37,7 +37,7 @@ class Obit < Formula
   depends_on "libair"
   depends_on "gcc"
 
-  conflicts_with "plplot", :because => "because it has a float type mismatch and configure can't disable it"
+  conflicts_with "plplot", :because => "it has a float type mismatch and configure can't disable it"
 
   # Build main Obit library as shared dylib
   # Improve installation procedure for Python module
@@ -140,25 +140,25 @@ index 40d2e7d..f8e4248 100644
  # targets to build
 -TARGETS =  libObit.a
 +TARGETS =  libObit.a libObit.dylib
- 
+
  # list of object modules
  OBJECTS := $(wildcard *.o)
- 
+
  all:  $(TARGETS)
- 
+
 -#  build Obit library
 +#  build Obit static library
  libObit.a: ${OBJECTS}
  	ar rv libObit.a ${OBJECTS}
  	${RANLIB} libObit.a
- 
+
 +# build Obit shared library
 +libObit.dylib: $(OBJECTS)
 +	$(CC) -dynamiclib -flat_namespace -undefined dynamic_lookup -o $@ $^
 +
  clean:
  	rm -f $(TARGETS)
- 
+
 diff --git a/Obit/tasks/Makefile.in b/Obit/tasks/Makefile.in
 index 798645f..be4db7d 100644
 --- a/Obit/tasks/Makefile.in
@@ -166,7 +166,7 @@ index 798645f..be4db7d 100644
 @@ -63,10 +63,16 @@ ALL_LDFLAGS = $(LDFLAGS) @CFITSIO_LDFLAGS@ @FFTW_LDFLAGS@  @FFTW3_LDFLAGS@  \
  	 @GSL_LDFLAGS@ @PLPLOT_LDFLAGS@ @PGPLOT_LDFLAGS@ @WVR_LDFLAGS@ \
  	$(CLIENT_LDFLAGS) $(SERVER_LDFLAGS)
- 
+
 -LIBS = ../lib/libObit.a @CFITSIO_LIBS@ @FFTW_LIBS@ @FFTW3_LIBS@ @GLIB_LIBS@ \
 -	@GSL_LIBS@ @PLPLOT_LIBS@ @PGPLOT_LIBS@ $(CLIENT_LIBS) $(SERVER_LIBS) \
 -	@LIBS@ @FLIBS@ @GTHREAD_LIBS@ @WVR_LIBS@
@@ -176,42 +176,42 @@ index 798645f..be4db7d 100644
 +# Shared library option
 +OBIT_LIB_TARGET = ../lib/libObit.dylib
 +OBIT_LIB = -L../lib -lObit
- 
+
 +LIBS = $(OBIT_LIB) @CFITSIO_LIBS@ @FFTW_LIBS@ @FFTW3_LIBS@ @GLIB_LIBS@ \
 +        @GSL_LIBS@ @PLPLOT_LIBS@ @PGPLOT_LIBS@ $(CLIENT_LIBS) $(SERVER_LIBS) \
 +        @LIBS@ @FLIBS@ @GTHREAD_LIBS@ @WVR_LIBS@
- 
+
  # get list of all c source files (*.c) files
  AllC    := $(wildcard *.c)
 @@ -76,17 +82,18 @@ TARGETS := $(addprefix $(BINDIR),$(EXECU))
  all: $(TARGETS)
- 
+
  # generic C compile/link
--$(TARGETS): $(BINDIR)% : %.c ../lib/libObit.a  
+-$(TARGETS): $(BINDIR)% : %.c ../lib/libObit.a
 +$(TARGETS): $(BINDIR)% : %.c $(OBIT_LIB_TARGET)
  	echo "compile $*.c"
  	$(CC) $(ALL_CPPFLAGS) $(ALL_CFLAGS) $(ALL_LDFLAGS) $*.c -o $* $(LIBS)
  	mv $* $(BINDIR)
- 
+
  # For specific executables
--$(EXECU): % : %.c ../lib/libObit.a  
+-$(EXECU): % : %.c ../lib/libObit.a
 +$(EXECU): % : %.c $(OBIT_LIB_TARGET)
  	$(CC) $(ALL_CPPFLAGS) $(ALL_CFLAGS) $(ALL_LDFLAGS) $< -o $* $(LIBS)
  	mv $* $(BINDIR)
- 
+
  clean:
  	rm -f $(TARGETS)
  	rm -f *.o
 +	rm -rf *.dSYM
- 
+
 diff --git a/Obit/python/Makefile.in b/Obit/python/Makefile.in
 index 5cf25a4..5dac1d6 100644
 --- a/Obit/python/Makefile.in
 +++ b/Obit/python/Makefile.in
 @@ -54,7 +54,12 @@ ALL_CFLAGS = $(CFLAGS) @GLIB_CFLAGS@ @GSL_CFLAGS@ @PLPLOT_CFLAGS@ \
  ALL_LDFLAGS = $(LDFLAGS) @CFITSIO_LDFLAGS@ @FFTW_LDFLAGS@  @FFTW3_LDFLAGS@  @GSL_LDFLAGS@ \
- 	@PLPLOT_LDFLAGS@ @PGPLOT_LDFLAGS@ @PYTHON_LDFLAGS@ 
- 
+ 	@PLPLOT_LDFLAGS@ @PGPLOT_LDFLAGS@ @PYTHON_LDFLAGS@
+
 -LIBS = ../lib/libObit.a @CFITSIO_LIBS@ @FFTW_LIBS@ @FFTW3_LIBS@ @GLIB_LIBS@ \
 +# Static library option
 +# OBIT_LIB = ../lib/libObit.a
@@ -220,29 +220,29 @@ index 5cf25a4..5dac1d6 100644
 +
 +LIBS = $(OBIT_LIB) @CFITSIO_LIBS@ @FFTW_LIBS@ @FFTW3_LIBS@ @GLIB_LIBS@ \
  	@GSL_LIBS@ @PLPLOT_LIBS@ @PGPLOT_LIBS@ @LIBS@ @FLIBS@ @GTHREAD_LIBS@
- 
- CLIENT_CPPFLAGS = $(ALL_CPPFLAGS) @XMLRPC_CLIENT_CPPFLAGS@ 
+
+ CLIENT_CPPFLAGS = $(ALL_CPPFLAGS) @XMLRPC_CLIENT_CPPFLAGS@
 @@ -71,12 +76,13 @@ SERVER_LIBS =  @XMLRPC_SERVER_LIBS@
  SWIG = @SWIG@
- 
+
  # Libraries in case they've changed
 -MYLIBS := $(wildcard ../lib/lib*.a)
 +# MYLIBS := $(wildcard ../lib/lib*.a)
 +MYLIBS := $(wildcard ../lib/lib*.dylib)
- 
+
  # Do everything in one big module
--TARGETS := Obit.so 
+-TARGETS := Obit.so
 +TARGETS := Obit.so
- 
+
 -all: $(TARGETS)
 +all: install
- 
+
  # Build shared library for python interface
  $(TARGETS): setupdata.py $(MYLIBS)
 @@ -84,6 +90,10 @@ $(TARGETS): setupdata.py $(MYLIBS)
  	python makesetup.py
  	python setup.py build install --install-lib=.
- 
+
 +install: $(TARGETS)
 +	mkdir -p build/site-packages
 +	cp *.py $(TARGETS) build/site-packages
@@ -255,14 +255,14 @@ index b76a1a4..d2667ac 100644
 --- a/Obit/Makefile.in
 +++ b/Obit/Makefile.in
 @@ -87,7 +87,7 @@ srcupdate:
- 
+
  # update library directory
- libupdate: 
+ libupdate:
 -	cd lib; $(MAKE) RANLIB="$(RANLIB)"
 +	cd lib; $(MAKE) RANLIB="$(RANLIB)" CC="$(CC)"
- 
+
  # update test software directory
- testupdate: 
+ testupdate:
 diff --git a/ObitTalk/python/Makefile.in b/ObitTalk/python/Makefile.in
 index d2a2b26..e479a41 100644
 --- a/ObitTalk/python/Makefile.in
@@ -270,26 +270,26 @@ index d2a2b26..e479a41 100644
 @@ -76,8 +76,8 @@ PROXYTAR:= $(DESTDIR)$(PYTHONDIR)/Proxy/AIPSData.py \
  WIZTAR:= $(DESTDIR)$(PYTHONDIR)/Wizardry/AIPSData.py \
  	$(DESTDIR)$(PYTHONDIR)/Wizardry/__init__.py
- 
+
 -# make all = directories
--all:  $(DESTDIR)$(PREFIX)/share $(DESTDIR)$(PREFIX)/share/obittalk 
+-all:  $(DESTDIR)$(PREFIX)/share $(DESTDIR)$(PREFIX)/share/obittalk
 +all:
 +	echo "Nothing to make."
- 
+
  install: $(PYTHONTAR) $(PROXYTAR) $(WIZTAR)
- 
+
 diff --git a/Obit/Makefile.in b/Obit/Makefile.in
 index d2667ac..bf2c392 100644
 --- a/Obit/Makefile.in
 +++ b/Obit/Makefile.in
 @@ -56,7 +56,7 @@ DISTRIB = @PACKAGE_TARNAME@@PACKAGE_VERSION@
  DIRN = @PACKAGE_NAME@
- 
+
  #------------------------------------------------------------------------
 -TARGETS = versionupdate cfitsioupdate xmlrpcupdate srcupdate libupdate \
 +TARGETS = cfitsioupdate xmlrpcupdate srcupdate libupdate \
  	pythonupdate taskupdate
- 
+
  all:  $(TARGETS)
 diff --git a/Obit/Makefile.in b/Obit/Makefile.in
 index bf2c392..2b27f6e 100644
@@ -297,31 +297,31 @@ index bf2c392..2b27f6e 100644
 +++ b/Obit/Makefile.in
 @@ -86,15 +86,15 @@ srcupdate:
  	cd src; $(MAKE)
- 
+
  # update library directory
--libupdate: 
+-libupdate:
 +libupdate: srcupdate
  	cd lib; $(MAKE) RANLIB="$(RANLIB)" CC="$(CC)"
- 
+
  # update test software directory
--testupdate: 
+-testupdate:
 +testupdate: libupdate
  	cd test; $(MAKE)
- 
+
  # update task software directory
--taskupdate: 
+-taskupdate:
 +taskupdate: libupdate
  	cd tasks; $(MAKE)
- 
+
  # update work directory
 @@ -102,7 +102,7 @@ work:
  	cd src/work; $(MAKE) CC="$(CC)" CFLAGS="$(CFLAGS)" LIB="$(LIB)"
- 
+
  # update python directory
--pythonupdate: 
+-pythonupdate:
 +pythonupdate: libupdate
  	cd python; $(MAKE)
- 
+
  # update from cvs repository
 diff --git a/ObitTalk/python/Makefile.in b/ObitTalk/python/Makefile.in
 index e479a41..b5eb152 100644
@@ -330,117 +330,117 @@ index e479a41..b5eb152 100644
 @@ -95,7 +95,7 @@ $(DESTDIR)$(PREFIX)/share: $(DESTDIR)$(PREFIX)
  $(DESTDIR)$(PREFIX)/share/obittalk: $(DESTDIR)$(PREFIX)/share
  	if test ! -d $(DESTDIR)$(PREFIX)/share/obittalk; then mkdir $(DESTDIR)$(PREFIX)/share/obittalk; fi
- 
+
 -$(DESTDIR)$(PYTHONDIR):$(DESTDIR)$(PREFIX)/share/obittalk
 +$(DESTDIR)$(PYTHONDIR):
  	if test ! -d $(DESTDIR)$(PYTHONDIR); then mkdir $(DESTDIR)$(PYTHONDIR); fi
- 
+
  $(DESTDIR)$(PROXYDIR):$(DESTDIR)$(PYTHONDIR)
 @@ -107,49 +107,49 @@ $(DESTDIR)$(WIZDIR):$(DESTDIR)$(PYTHONDIR)
- $(DESTDIR)$(PYTHONDIR)/AIPSData.py: AIPSData.py $(DESTDIR)$(PYTHONDIR) 
+ $(DESTDIR)$(PYTHONDIR)/AIPSData.py: AIPSData.py $(DESTDIR)$(PYTHONDIR)
  	cp AIPSData.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/AIPS.py: AIPS.py
 +$(DESTDIR)$(PYTHONDIR)/AIPS.py: AIPS.py $(DESTDIR)$(PYTHONDIR)
  	cp AIPS.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/AIPSTask.py: AIPSTask.py
 +$(DESTDIR)$(PYTHONDIR)/AIPSTask.py: AIPSTask.py $(DESTDIR)$(PYTHONDIR)
  	cp AIPSTask.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/AIPSTV.py: AIPSTV.py
 +$(DESTDIR)$(PYTHONDIR)/AIPSTV.py: AIPSTV.py $(DESTDIR)$(PYTHONDIR)
  	cp AIPSTV.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/AIPSUtil.py: AIPSUtil.py
 +$(DESTDIR)$(PYTHONDIR)/AIPSUtil.py: AIPSUtil.py $(DESTDIR)$(PYTHONDIR)
  	cp AIPSUtil.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/FITSData.py: FITSData.py
 +$(DESTDIR)$(PYTHONDIR)/FITSData.py: FITSData.py $(DESTDIR)$(PYTHONDIR)
  	cp FITSData.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/FITS.py: FITS.py
 +$(DESTDIR)$(PYTHONDIR)/FITS.py: FITS.py $(DESTDIR)$(PYTHONDIR)
  	cp FITS.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/LocalProxy.py: LocalProxy.py
 +$(DESTDIR)$(PYTHONDIR)/LocalProxy.py: LocalProxy.py $(DESTDIR)$(PYTHONDIR)
  	cp LocalProxy.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/MinimalMatch.py: MinimalMatch.py
 +$(DESTDIR)$(PYTHONDIR)/MinimalMatch.py: MinimalMatch.py $(DESTDIR)$(PYTHONDIR)
  	cp MinimalMatch.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/ObitTalk.py: ObitTalk.py
 +$(DESTDIR)$(PYTHONDIR)/ObitTalk.py: ObitTalk.py $(DESTDIR)$(PYTHONDIR)
  	cp ObitTalk.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/ObitTalkUtil.py: ObitTalkUtil.py
 +$(DESTDIR)$(PYTHONDIR)/ObitTalkUtil.py: ObitTalkUtil.py $(DESTDIR)$(PYTHONDIR)
  	cp ObitTalkUtil.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/ObitTask.py: ObitTask.py
 +$(DESTDIR)$(PYTHONDIR)/ObitTask.py: ObitTask.py $(DESTDIR)$(PYTHONDIR)
  	cp ObitTask.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/ObitScript.py: ObitScript.py
 +$(DESTDIR)$(PYTHONDIR)/ObitScript.py: ObitScript.py $(DESTDIR)$(PYTHONDIR)
  	cp ObitScript.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/otcompleter.py: otcompleter.py
 +$(DESTDIR)$(PYTHONDIR)/otcompleter.py: otcompleter.py $(DESTDIR)$(PYTHONDIR)
  	cp otcompleter.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Task.py: Task.py
 +$(DESTDIR)$(PYTHONDIR)/Task.py: Task.py $(DESTDIR)$(PYTHONDIR)
  	cp Task.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/XMLRPCServer.py: XMLRPCServer.py
 +$(DESTDIR)$(PYTHONDIR)/XMLRPCServer.py: XMLRPCServer.py $(DESTDIR)$(PYTHONDIR)
  	cp XMLRPCServer.py $@
- 
- 
+
+
 @@ -160,32 +160,32 @@ $(DESTDIR)$(PYTHONDIR)/Proxy/AIPSData.py: Proxy/AIPSData.py $(DESTDIR)$(PROXYDIR
- $(DESTDIR)$(PYTHONDIR)/Proxy/FITSData.py: Proxy/FITSData.py $(DESTDIR)$(PROXYDIR) 
+ $(DESTDIR)$(PYTHONDIR)/Proxy/FITSData.py: Proxy/FITSData.py $(DESTDIR)$(PROXYDIR)
  	cp ./Proxy/FITSData.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Proxy/AIPS.py: Proxy/AIPS.py
 +$(DESTDIR)$(PYTHONDIR)/Proxy/AIPS.py: Proxy/AIPS.py $(DESTDIR)$(PROXYDIR)
  	cp ./Proxy/AIPS.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Proxy/AIPSTask.py: Proxy/AIPSTask.py
 +$(DESTDIR)$(PYTHONDIR)/Proxy/AIPSTask.py: Proxy/AIPSTask.py $(DESTDIR)$(PROXYDIR)
  	cp ./Proxy/AIPSTask.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Proxy/__init__.py: Proxy/__init__.py
 +$(DESTDIR)$(PYTHONDIR)/Proxy/__init__.py: Proxy/__init__.py $(DESTDIR)$(PROXYDIR)
  	cp ./Proxy/__init__.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Proxy/ObitTask.py: Proxy/ObitTask.py
 +$(DESTDIR)$(PYTHONDIR)/Proxy/ObitTask.py: Proxy/ObitTask.py $(DESTDIR)$(PROXYDIR)
  	cp ./Proxy/ObitTask.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Proxy/ObitScriptP.py: Proxy/ObitScriptP.py
 +$(DESTDIR)$(PYTHONDIR)/Proxy/ObitScriptP.py: Proxy/ObitScriptP.py $(DESTDIR)$(PROXYDIR)
  	cp ./Proxy/ObitScriptP.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Proxy/Popsdat.py: Proxy/Popsdat.py
 +$(DESTDIR)$(PYTHONDIR)/Proxy/Popsdat.py: Proxy/Popsdat.py $(DESTDIR)$(PROXYDIR)
  	cp ./Proxy/Popsdat.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Proxy/Task.py: Proxy/Task.py
 +$(DESTDIR)$(PYTHONDIR)/Proxy/Task.py: Proxy/Task.py $(DESTDIR)$(PROXYDIR)
  	cp ./Proxy/Task.py $@
- 
+
  # Wizardry
- $(DESTDIR)$(PYTHONDIR)/Wizardry/AIPSData.py: Wizardry/AIPSData.py $(DESTDIR)$(WIZDIR) 
+ $(DESTDIR)$(PYTHONDIR)/Wizardry/AIPSData.py: Wizardry/AIPSData.py $(DESTDIR)$(WIZDIR)
  	cp ./Wizardry/AIPSData.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Wizardry/__init__.py: Wizardry/__init__.py
 +$(DESTDIR)$(PYTHONDIR)/Wizardry/__init__.py: Wizardry/__init__.py $(DESTDIR)$(WIZDIR)
  	cp ./Wizardry/__init__.py $@
- 
+
  clean:
 diff --git a/ObitView/Makefile.in b/ObitView/Makefile.in
 index fbfa6cb..ec98c8a 100644
@@ -448,61 +448,61 @@ index fbfa6cb..ec98c8a 100644
 +++ b/ObitView/Makefile.in
 @@ -88,47 +88,47 @@ CLIENT_LIBS = lib/libObitView.a @MOTIF_LIBS@ @X_LIBS@ @OBIT_LIBS@  @GLIB_LIBS@ \
  all:  $(TARGETS)
- 
+
  # update source/object directory
--srcupdate: 
+-srcupdate:
 +srcupdate: src/*.c
  	cd src; $(MAKE)
- 
+
  # update library directory
--libupdate: 
+-libupdate:
 +libupdate: srcupdate
  	cd lib; $(MAKE) RANLIB="$(RANLIB)"
- 
+
  # Link ObitView
--ObitView: src/*.c  srcupdate ObitView.c 
-+ObitView: libupdate ObitView.c 
+-ObitView: src/*.c  srcupdate ObitView.c
++ObitView: libupdate ObitView.c
  	$(CC) ObitView.c -o ObitView  $(SERVER_CFLAGS) $(SERVER_CPPFLAGS) \
- 	$(SERVER_LDFLAGS) $(SERVER_LIBS) $(CLIENT_LIBS) 
- 
+ 	$(SERVER_LDFLAGS) $(SERVER_LIBS) $(CLIENT_LIBS)
+
  # Link ObitMess
--ObitMess: src/*.c  srcupdate ObitMess.c 
-+ObitMess: libupdate ObitMess.c 
+-ObitMess: src/*.c  srcupdate ObitMess.c
++ObitMess: libupdate ObitMess.c
  	$(CC) ObitMess.c -o ObitMess  $(SERVER_CFLAGS) $(SERVER_CPPFLAGS) \
- 	$(SERVER_LDFLAGS) $(SERVER_LIBS) $(CLIENT_LIBS) 
- 
+ 	$(SERVER_LDFLAGS) $(SERVER_LIBS) $(CLIENT_LIBS)
+
  # Link clientTest
--clientTest:   clientTest.c 
-+clientTest: libupdate clientTest.c 
+-clientTest:   clientTest.c
++clientTest: libupdate clientTest.c
  	$(CC) clientTest.c -o clientTest  -Iinclude $(CLIENT_CFLAGS) \
  	$(CLIENT_CPPFLAGS) $(CLIENT_LDFLAGS) \
  	$(CLIENT_LIBS)
- 
+
  # Link clientFCopy
--clientFCopy:   clientFCopy.c 
-+clientFCopy: libupdate clientFCopy.c 
+-clientFCopy:   clientFCopy.c
++clientFCopy: libupdate clientFCopy.c
  	$(CC) clientFCopy.c -o clientFCopy  -Iinclude $(CLIENT_CFLAGS) \
  	$(CLIENT_CPPFLAGS) $(CLIENT_LDFLAGS) \
  	$(CLIENT_LIBS)
- 
+
  # test run ObitView
--testObitView: 
+-testObitView:
 +testObitView: ObitView
  	ObitView aaaSomeFile.fits
- 
+
  # test run ObitMess
--testObitMess: 
+-testObitMess:
 +testObitMess: ObitMess
  	ObitMess &
  	# Need to wait to start
  	python testObitMess.py
- 
+
  # Copy to where it should go
 -install: @exec_prefix@/bin
 +install: @exec_prefix@/bin ObitView ObitMess
  	@install_sh@ -s ObitView @exec_prefix@/bin/
  	@install_sh@ -s ObitMess @exec_prefix@/bin/
- 
+
 diff --git a/Obit/python/makesetup.py b/Obit/python/makesetup.py
 index 58740a0..5e9e2c2 100644
 --- a/Obit/python/makesetup.py
