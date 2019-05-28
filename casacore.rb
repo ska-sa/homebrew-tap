13426d2626e21401981ef5452844625b5ab46c69
Ludwig Schwardt <ludwig@ska.ac.za>
2019-05-28 17:02:26 +0200

    casacore: Fix HEAD and unify patch for v3.1.0
    The HEAD no longer needs a patch (although it is currently running off
    a feature branch to be merged as casacore/casacore#922). Now use a
    single patch that turns v3.1.0 into HEAD (at least as far as the
    Python build scripts are concerned).

--- casacore.rb
+++ casacore.rb
@@ -3,7 +3,7 @@ class Casacore < Formula
   homepage "https://github.com/casacore/casacore"
   url "https://github.com/casacore/casacore/archive/v3.1.0.tar.gz"
   sha256 "a6adf2d77ad0d6f32995b1e297fd88d31ded9c3e0bb8f28966d7b35a969f7897"
-  head "https://github.com/casacore/casacore.git"
+  head "https://github.com/casacore/casacore.git", :branch => "improve-cmake-findpython"
 
   depends_on "cmake" => :build
   depends_on "cfitsio"
@@ -23,16 +23,11 @@ class Casacore < Formula
 
   stable do
     patch do
-      # casacore/casacore#846: Boost Python upstream fix (remove on next release)
-      url "https://gist.githubusercontent.com/ludwigschwardt/bfbe9dd2538abbbf22552fde40bec935/raw/250aff71b76bb7851b02faa88f99642d55f5db44/casacore-patch-boost-pythonxy.patch"
-      sha256 "99661f5f9132dae77bc83ae1d1d01785c0ab40a1b78956d3304d243532370784"
+      # casacore/casacore#846: Boost Python upstream fix
+      # Use FindPython2 and FindPython3 modules introduced in cmake 3.12
+      url "https://gist.githubusercontent.com/ludwigschwardt/f22e09d458aebd84c59de3013d0671ec/raw/6105f7497548a74dbe24285884cfda6127ca5526/casacore-improve-cmake-findpython-3.1.0.patch"
+      sha256 "ecd3526240ee31896afee31f1c2d8e6fb749bca9c2eeb4e578d2f3dcd217c12c"
     end
-  end
-
-  patch do
-    # Use FindPython2 and FindPython3 modules introduced in cmake 3.12
-    url "https://gist.githubusercontent.com/ludwigschwardt/0bfaef7b2c6832fb018332742e14924e/raw/85002935f06cc821b9bda8246f5a950220e23f9e/casacore-cmake-findpython.patch"
-    sha256 "602a5e4728e972167575f39cfc451d91a0c9121b1b247b6eff8e480ccb96b791"
   end
 
   def install
