db1bc17c76e968759eb09eaf21ee1c86e28c5821
Ludwig Schwardt <ludwig@ska.ac.za>
2019-06-25 17:03:16 +0200

    casacore: don't link against libpython
    This is generally a bad idea - see e.g. this post:
    https://blog.tim-smith.us/2015/09/python-extension-modules-os-x/
    
    It prevented Python wheels compiled on a macOS 10.10 build system,
    with embedded casacore dylibs produced by this formula, from importing
    on another system (kinda the whole idea!). Also note the difference
    between a shared library (libcasa_python) and a module (tConvert),
    which are treated differently by CMake and have their own flags.

--- casacore.rb
+++ casacore.rb
@@ -21,6 +21,8 @@ class Casacore < Formula
     depends_on "boost-python3"
   end
 
+  patch :DATA
+
   def install
     # To get a build type besides "release" we need to change from superenv to std env first
     build_type = "release"
@@ -30,6 +32,7 @@ class Casacore < Formula
     cmake_args.delete "-DCMAKE_BUILD_TYPE=None"
     cmake_args << "-DCMAKE_BUILD_TYPE=#{build_type}"
     cmake_args << "-DCMAKE_SHARED_LINKER_FLAGS='-undefined dynamic_lookup'"
+    cmake_args << "-DCMAKE_MODULE_LINKER_FLAGS='-undefined dynamic_lookup'"
     cmake_args << "-DBUILD_PYTHON=ON"
     cmake_args << "-DBUILD_PYTHON3=ON" if build.with? "python"
     cmake_args << "-DUSE_FFTW3=ON" << "-DFFTW3_ROOT_DIR=#{HOMEBREW_PREFIX}"
@@ -44,3 +47,43 @@ class Casacore < Formula
     system bin/"findmeastable", "DE405"
   end
 end
+
+__END__
+diff --git a/python/CMakeLists-cmake3.12.txt b/python/CMakeLists-cmake3.12.txt
+index 7c981f7..49c89cc 100644
+--- a/python/CMakeLists-cmake3.12.txt
++++ b/python/CMakeLists-cmake3.12.txt
+@@ -60,7 +60,7 @@ Converters/PycValueHolder.h
+ Converters/PycArray.tcc
+ )
+ 
+-target_link_libraries (casa_python casa_casa ${PYTHON2_Boost_LIBRARIES} ${PYTHON2_LIBRARIES} ${CASACORE_ARCH_LIBS})
++target_link_libraries (casa_python casa_casa ${PYTHON2_Boost_LIBRARIES} ${CASACORE_ARCH_LIBS})
+ 
+ install (TARGETS casa_python
+ RUNTIME DESTINATION bin
+diff --git a/python3/CMakeLists-cmake3.12.txt b/python3/CMakeLists-cmake3.12.txt
+index 26d5ca5..8db7fec 100644
+--- a/python3/CMakeLists-cmake3.12.txt
++++ b/python3/CMakeLists-cmake3.12.txt
+@@ -51,7 +51,7 @@ add_library (casa_python3
+ )
+ 
+ 
+-target_link_libraries (casa_python3 casa_casa ${PYTHON3_Boost_LIBRARIES} ${PYTHON3_LIBRARIES})
++target_link_libraries (casa_python3 casa_casa ${PYTHON3_Boost_LIBRARIES})
+ 
+ install (TARGETS casa_python3
+ RUNTIME DESTINATION bin
+diff --git a/python/Converters/test/CMakeLists.txt b/python/Converters/test/CMakeLists.txt
+index 32214a8..321c98b 100644
+--- a/python/Converters/test/CMakeLists.txt
++++ b/python/Converters/test/CMakeLists.txt
+@@ -1,6 +1,6 @@
+ include_directories ("..")
+ add_library(tConvert MODULE tConvert.cc)
+ SET_TARGET_PROPERTIES(tConvert PROPERTIES PREFIX "_") 
+-target_link_libraries (tConvert casa_python ${PYTHON_LIBRARIES})
++target_link_libraries (tConvert casa_python)
+ add_test (tConvert ${CMAKE_SOURCE_DIR}/cmake/cmake_assay ./tConvert)
+ add_dependencies(check tConvert)
