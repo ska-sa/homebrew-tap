c24839f91d1d95ddb0beb8f8110de84b9b25a82d
Ludwig Schwardt <ludwig@ska.ac.za>
2023-02-16 18:31:35 +0200

    obit: Fix up patches for latest Obit
    Most of the hunks failed due to whitespace creeping into the source.
    
    One of the hunks in /trunk/ObitSystem/Obit/python/Makefile.in even
    turned out to be obsolete, since Bill now uses a shared library too
    instead of the previous static library.

--- obit.rb
+++ obit.rb
@@ -171,7 +171,7 @@ index a7995bd..1884fec 100644
 @@ -63,7 +63,14 @@ ALL_LDFLAGS = $(LDFLAGS) @CFITSIO_LDFLAGS@ @FFTW_LDFLAGS@  @FFTW3_LDFLAGS@  \
  	 @GSL_LDFLAGS@ @PLPLOT_LDFLAGS@ @PGPLOT_LDFLAGS@ @WVR_LDFLAGS@ \
  	$(CLIENT_LDFLAGS) $(SERVER_LDFLAGS)
- 
+
 -LIBS = ../lib/libObit.a @CFITSIO_LIBS@ @FFTW_LIBS@ @FFTW3_LIBS@ @GLIB_LIBS@ \
 +# Static library option
 +# OBIT_LIB_TARGET = ../lib/libObit.a
@@ -183,84 +183,63 @@ index a7995bd..1884fec 100644
 +LIBS = $(OBIT_LIB) @CFITSIO_LIBS@ @FFTW_LIBS@ @FFTW3_LIBS@ @GLIB_LIBS@ \
  	@GSL_LIBS@ @PLPLOT_LIBS@ @PGPLOT_LIBS@ $(CLIENT_LIBS) $(SERVER_LIBS) \
  	@LIBS@ @FLIBS@ @GTHREAD_LIBS@ @WVR_LIBS@
- 
+
 @@ -76,17 +83,18 @@ TARGETS := $(addprefix $(BINDIR),$(EXECU))
  all: $(TARGETS)
- 
+
  # generic C compile/link
 -$(TARGETS): $(BINDIR)% : %.c ../lib/libObit.a  
 +$(TARGETS): $(BINDIR)% : %.c $(OBIT_LIB_TARGET)
  	echo "compile $*.c"
  	$(CC) $(ALL_CPPFLAGS) $(ALL_CFLAGS) $(ALL_LDFLAGS) $*.c -o $* $(LIBS)
  	mv $* $(BINDIR)
- 
+
  # For specific executables
 -$(EXECU): % : %.c ../lib/libObit.a  
 +$(EXECU): % : %.c $(OBIT_LIB_TARGET)
  	$(CC) $(ALL_CPPFLAGS) $(ALL_CFLAGS) $(ALL_LDFLAGS) $< -o $* $(LIBS)
  	mv $* $(BINDIR)
- 
+
  clean:
  	rm -f $(TARGETS)
  	rm -f *.o
 +	rm -rf *.dSYM
- 
+
 diff --git a/trunk/ObitSystem/Obit/python/Makefile.in b/trunk/ObitSystem/Obit/python/Makefile.in
 index 695f215..e6dd7f4 100644
 --- a/trunk/ObitSystem/Obit/python/Makefile.in
 +++ b/trunk/ObitSystem/Obit/python/Makefile.in
-@@ -53,8 +53,12 @@ ALL_CFLAGS = $(CFLAGS) @GLIB_CFLAGS@ @GSL_CFLAGS@ @PLPLOT_CFLAGS@ \
- 	@PGPLOT_CFLAGS@ 
- ALL_LDFLAGS = $(LDFLAGS) @CFITSIO_LDFLAGS@ @FFTW_LDFLAGS@  @FFTW3_LDFLAGS@  @GSL_LDFLAGS@ \
- 	@PLPLOT_LDFLAGS@ @PGPLOT_LDFLAGS@ @PYTHON_LDFLAGS@ 
-+# Static library option
-+# OBIT_LIB = ../lib/libObit.a
-+# Shared library option
-+OBIT_LIB = -L../lib -lObit
- 
--LIBS = ../lib/libObit.a @CFITSIO_LIBS@ @FFTW_LIBS@ @FFTW3_LIBS@ @GLIB_LIBS@ \
-+LIBS = $(OBIT_LIB) @CFITSIO_LIBS@ @FFTW_LIBS@ @FFTW3_LIBS@ @GLIB_LIBS@ \
- 	@GSL_LIBS@ @PLPLOT_LIBS@ @PGPLOT_LIBS@ @LIBS@ @FLIBS@ @GTHREAD_LIBS@
- 
- CLIENT_CPPFLAGS = $(ALL_CPPFLAGS) @XMLRPC_CLIENT_CPPFLAGS@ 
-@@ -71,12 +75,13 @@ SERVER_LIBS =  @XMLRPC_SERVER_LIBS@
- SWIG = @SWIG@
- 
+@@ -72,12 +76,17 @@ SWIG = @SWIG@
+ SWIGLIB	   = 
+
  # Libraries in case they've changed
 -MYLIBS := $(wildcard ../lib/lib*.a)
 +# MYLIBS := $(wildcard ../lib/lib*.a)
 +MYLIBS := $(wildcard ../lib/lib*.dylib)
- 
+
  # Do everything in one big module
- TARGETS := Obit.so 
- 
+ TARGETS := Obit 
+
 -all: $(TARGETS)
 +all: install
- 
- # Build shared library for python interface
- $(TARGETS): setupdata.py $(MYLIBS)
-@@ -84,6 +89,10 @@ $(TARGETS): setupdata.py $(MYLIBS)
- 	python makesetup.py
- 	python setup.py build install --install-lib=.
- 
++
 +install: $(TARGETS)
 +	mkdir -p build/site-packages
 +	cp *.py $(TARGETS) build/site-packages
-+
- # Build python/Obit interface
- interface: Obit_wrap.c
- 	echo "rebuild Obit/python interface"
+
+ # Build shared library for python interface
+ $(TARGETS): setupdata.py $(MYLIBS)
 diff --git a/trunk/ObitSystem/Obit/Makefile.in b/trunk/ObitSystem/Obit/Makefile.in
 index 7b03cca..d11461b 100644
 --- a/trunk/ObitSystem/Obit/Makefile.in
 +++ b/trunk/ObitSystem/Obit/Makefile.in
-@@ -87,7 +87,7 @@ srcupdate:
- 
+@@ -87,7 +87,7 @@ 	cd src; $(MAKE)
+
  # update library directory
  libupdate: 
 -	cd lib; $(MAKE) RANLIB="$(RANLIB)"
 +	cd lib; $(MAKE) RANLIB="$(RANLIB)" CC="$(CC)"
- 
+
  # update test software directory
  testupdate: 
 diff --git a/trunk/ObitSystem/ObitTalk/python/Makefile.in b/trunk/ObitSystem/ObitTalk/python/Makefile.in
@@ -270,26 +249,26 @@ index 85e49b7..a68c62a 100644
 @@ -76,8 +76,8 @@ PROXYTAR:= $(DESTDIR)$(PYTHONDIR)/Proxy/AIPSData.py \
  WIZTAR:= $(DESTDIR)$(PYTHONDIR)/Wizardry/AIPSData.py \
  	$(DESTDIR)$(PYTHONDIR)/Wizardry/__init__.py
- 
+
 -# make all = directories
 -all:  $(DESTDIR)$(PREFIX)/share $(DESTDIR)$(PREFIX)/share/obittalk 
 +all:
 +	echo "Nothing to make."
- 
+
  install: $(PYTHONTAR) $(PROXYTAR) $(WIZTAR)
- 
+
 diff --git a/trunk/ObitSystem/Obit/Makefile.in b/trunk/ObitSystem/Obit/Makefile.in
 index d11461b..f2fdfb0 100644
 --- a/trunk/ObitSystem/Obit/Makefile.in
 +++ b/trunk/ObitSystem/Obit/Makefile.in
 @@ -56,7 +56,7 @@ DISTRIB = @PACKAGE_TARNAME@@PACKAGE_VERSION@
  DIRN = @PACKAGE_NAME@
- 
+
  #------------------------------------------------------------------------
 -TARGETS = versionupdate cfitsioupdate xmlrpcupdate srcupdate libupdate \
 +TARGETS = cfitsioupdate xmlrpcupdate srcupdate libupdate \
  	pythonupdate taskupdate
- 
+
  all:  $(TARGETS)
 diff --git a/trunk/ObitSystem/Obit/Makefile.in b/trunk/ObitSystem/Obit/Makefile.in
 index f2fdfb0..296094e 100644
@@ -297,31 +276,31 @@ index f2fdfb0..296094e 100644
 +++ b/trunk/ObitSystem/Obit/Makefile.in
 @@ -86,15 +86,15 @@ srcupdate:
  	cd src; $(MAKE)
- 
+
  # update library directory
 -libupdate: 
 +libupdate: srcupdate
  	cd lib; $(MAKE) RANLIB="$(RANLIB)" CC="$(CC)"
- 
+
  # update test software directory
 -testupdate: 
 +testupdate: libupdate
  	cd test; $(MAKE)
- 
+
  # update task software directory
 -taskupdate: 
 +taskupdate: libupdate
  	cd tasks; $(MAKE)
- 
+
  # update work directory
-@@ -102,7 +102,7 @@ work:
+@@ -102,7 +102,7 @@ work: 
  	cd src/work; $(MAKE) CC="$(CC)" CFLAGS="$(CFLAGS)" LIB="$(LIB)"
- 
+
  # update python directory
 -pythonupdate: 
 +pythonupdate: libupdate
  	cd python; $(MAKE)
- 
+
  # update from cvs repository
 diff --git a/trunk/ObitSystem/ObitTalk/python/Makefile.in b/trunk/ObitSystem/ObitTalk/python/Makefile.in
 index a68c62a..7dd5e90 100644
@@ -330,117 +309,117 @@ index a68c62a..7dd5e90 100644
 @@ -95,7 +95,7 @@ $(DESTDIR)$(PREFIX)/share: $(DESTDIR)$(PREFIX)
  $(DESTDIR)$(PREFIX)/share/obittalk: $(DESTDIR)$(PREFIX)/share
  	if test ! -d $(DESTDIR)$(PREFIX)/share/obittalk; then mkdir $(DESTDIR)$(PREFIX)/share/obittalk; fi
- 
+
 -$(DESTDIR)$(PYTHONDIR):$(DESTDIR)$(PREFIX)/share/obittalk
 +$(DESTDIR)$(PYTHONDIR):
  	if test ! -d $(DESTDIR)$(PYTHONDIR); then mkdir $(DESTDIR)$(PYTHONDIR); fi
- 
+
  $(DESTDIR)$(PROXYDIR):$(DESTDIR)$(PYTHONDIR)
 @@ -107,49 +107,49 @@ $(DESTDIR)$(WIZDIR):$(DESTDIR)$(PYTHONDIR)
  $(DESTDIR)$(PYTHONDIR)/AIPSData.py: AIPSData.py $(DESTDIR)$(PYTHONDIR) 
  	cp AIPSData.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/AIPS.py: AIPS.py
 +$(DESTDIR)$(PYTHONDIR)/AIPS.py: AIPS.py $(DESTDIR)$(PYTHONDIR)
  	cp AIPS.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/AIPSTask.py: AIPSTask.py
 +$(DESTDIR)$(PYTHONDIR)/AIPSTask.py: AIPSTask.py $(DESTDIR)$(PYTHONDIR)
  	cp AIPSTask.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/AIPSTV.py: AIPSTV.py
 +$(DESTDIR)$(PYTHONDIR)/AIPSTV.py: AIPSTV.py $(DESTDIR)$(PYTHONDIR)
  	cp AIPSTV.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/AIPSUtil.py: AIPSUtil.py
 +$(DESTDIR)$(PYTHONDIR)/AIPSUtil.py: AIPSUtil.py $(DESTDIR)$(PYTHONDIR)
  	cp AIPSUtil.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/FITSData.py: FITSData.py
 +$(DESTDIR)$(PYTHONDIR)/FITSData.py: FITSData.py $(DESTDIR)$(PYTHONDIR)
  	cp FITSData.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/FITS.py: FITS.py
 +$(DESTDIR)$(PYTHONDIR)/FITS.py: FITS.py $(DESTDIR)$(PYTHONDIR)
  	cp FITS.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/LocalProxy.py: LocalProxy.py
 +$(DESTDIR)$(PYTHONDIR)/LocalProxy.py: LocalProxy.py $(DESTDIR)$(PYTHONDIR)
  	cp LocalProxy.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/MinimalMatch.py: MinimalMatch.py
 +$(DESTDIR)$(PYTHONDIR)/MinimalMatch.py: MinimalMatch.py $(DESTDIR)$(PYTHONDIR)
  	cp MinimalMatch.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/ObitTalk.py: ObitTalk.py
 +$(DESTDIR)$(PYTHONDIR)/ObitTalk.py: ObitTalk.py $(DESTDIR)$(PYTHONDIR)
  	cp ObitTalk.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/ObitTalkUtil.py: ObitTalkUtil.py
 +$(DESTDIR)$(PYTHONDIR)/ObitTalkUtil.py: ObitTalkUtil.py $(DESTDIR)$(PYTHONDIR)
  	cp ObitTalkUtil.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/ObitTask.py: ObitTask.py
 +$(DESTDIR)$(PYTHONDIR)/ObitTask.py: ObitTask.py $(DESTDIR)$(PYTHONDIR)
  	cp ObitTask.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/ObitScript.py: ObitScript.py
 +$(DESTDIR)$(PYTHONDIR)/ObitScript.py: ObitScript.py $(DESTDIR)$(PYTHONDIR)
  	cp ObitScript.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/otcompleter.py: otcompleter.py
 +$(DESTDIR)$(PYTHONDIR)/otcompleter.py: otcompleter.py $(DESTDIR)$(PYTHONDIR)
  	cp otcompleter.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Task.py: Task.py
 +$(DESTDIR)$(PYTHONDIR)/Task.py: Task.py $(DESTDIR)$(PYTHONDIR)
  	cp Task.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/XMLRPCServer.py: XMLRPCServer.py
 +$(DESTDIR)$(PYTHONDIR)/XMLRPCServer.py: XMLRPCServer.py $(DESTDIR)$(PYTHONDIR)
  	cp XMLRPCServer.py $@
- 
- 
+
+
 @@ -160,32 +160,32 @@ $(DESTDIR)$(PYTHONDIR)/Proxy/AIPSData.py: Proxy/AIPSData.py $(DESTDIR)$(PROXYDIR
  $(DESTDIR)$(PYTHONDIR)/Proxy/FITSData.py: Proxy/FITSData.py $(DESTDIR)$(PROXYDIR) 
  	cp ./Proxy/FITSData.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Proxy/AIPS.py: Proxy/AIPS.py
 +$(DESTDIR)$(PYTHONDIR)/Proxy/AIPS.py: Proxy/AIPS.py $(DESTDIR)$(PROXYDIR)
  	cp ./Proxy/AIPS.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Proxy/AIPSTask.py: Proxy/AIPSTask.py
 +$(DESTDIR)$(PYTHONDIR)/Proxy/AIPSTask.py: Proxy/AIPSTask.py $(DESTDIR)$(PROXYDIR)
  	cp ./Proxy/AIPSTask.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Proxy/__init__.py: Proxy/__init__.py
 +$(DESTDIR)$(PYTHONDIR)/Proxy/__init__.py: Proxy/__init__.py $(DESTDIR)$(PROXYDIR)
  	cp ./Proxy/__init__.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Proxy/ObitTask.py: Proxy/ObitTask.py
 +$(DESTDIR)$(PYTHONDIR)/Proxy/ObitTask.py: Proxy/ObitTask.py $(DESTDIR)$(PROXYDIR)
  	cp ./Proxy/ObitTask.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Proxy/ObitScriptP.py: Proxy/ObitScriptP.py
 +$(DESTDIR)$(PYTHONDIR)/Proxy/ObitScriptP.py: Proxy/ObitScriptP.py $(DESTDIR)$(PROXYDIR)
  	cp ./Proxy/ObitScriptP.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Proxy/Popsdat.py: Proxy/Popsdat.py
 +$(DESTDIR)$(PYTHONDIR)/Proxy/Popsdat.py: Proxy/Popsdat.py $(DESTDIR)$(PROXYDIR)
  	cp ./Proxy/Popsdat.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Proxy/Task.py: Proxy/Task.py
 +$(DESTDIR)$(PYTHONDIR)/Proxy/Task.py: Proxy/Task.py $(DESTDIR)$(PROXYDIR)
  	cp ./Proxy/Task.py $@
- 
+
  # Wizardry
  $(DESTDIR)$(PYTHONDIR)/Wizardry/AIPSData.py: Wizardry/AIPSData.py $(DESTDIR)$(WIZDIR) 
  	cp ./Wizardry/AIPSData.py $@
- 
+
 -$(DESTDIR)$(PYTHONDIR)/Wizardry/__init__.py: Wizardry/__init__.py
 +$(DESTDIR)$(PYTHONDIR)/Wizardry/__init__.py: Wizardry/__init__.py $(DESTDIR)$(WIZDIR)
  	cp ./Wizardry/__init__.py $@
- 
+
  clean:
 diff --git a/trunk/ObitSystem/ObitView/Makefile.in b/trunk/ObitSystem/ObitView/Makefile.in
 index 433277e..0276e5c 100644
@@ -448,66 +427,66 @@ index 433277e..0276e5c 100644
 +++ b/trunk/ObitSystem/ObitView/Makefile.in
 @@ -88,47 +88,47 @@ CLIENT_LIBS = lib/libObitView.a @MOTIF_LIBS@ @X_LIBS@ @OBIT_LIBS@  @GLIB_LIBS@ \
  all:  $(TARGETS)
- 
+
  # update source/object directory
 -srcupdate: 
 +srcupdate: src/*.c
  	cd src; $(MAKE)
- 
+
  # update library directory
 -libupdate: 
 +libupdate: srcupdate
  	cd lib; $(MAKE) RANLIB="$(RANLIB)"
- 
+
  # Link ObitView
 -ObitView: src/*.c  srcupdate ObitView.c 
 +ObitView: libupdate ObitView.c
  	$(CC) ObitView.c -o ObitView  $(SERVER_CFLAGS) $(SERVER_CPPFLAGS) \
  	$(SERVER_LDFLAGS) $(SERVER_LIBS) $(CLIENT_LIBS) 
- 
+
  # Link ObitMess
 -ObitMess: src/*.c  srcupdate ObitMess.c 
 +ObitMess: libupdate ObitMess.c
  	$(CC) ObitMess.c -o ObitMess  $(SERVER_CFLAGS) $(SERVER_CPPFLAGS) \
  	$(SERVER_LDFLAGS) $(SERVER_LIBS) $(CLIENT_LIBS) 
- 
+
  # Link clientTest
 -clientTest:   clientTest.c 
 +clientTest: libupdate clientTest.c
  	$(CC) clientTest.c -o clientTest  -Iinclude $(CLIENT_CFLAGS) \
  	$(CLIENT_CPPFLAGS) $(CLIENT_LDFLAGS) \
  	$(CLIENT_LIBS)
- 
+
  # Link clientFCopy
 -clientFCopy:   clientFCopy.c 
 +clientFCopy: libupdate clientFCopy.c
  	$(CC) clientFCopy.c -o clientFCopy  -Iinclude $(CLIENT_CFLAGS) \
  	$(CLIENT_CPPFLAGS) $(CLIENT_LDFLAGS) \
  	$(CLIENT_LIBS)
- 
+
  # test run ObitView
 -testObitView: 
 +testObitView: ObitView
  	ObitView aaaSomeFile.fits
- 
+
  # test run ObitMess
 -testObitMess: 
 +testObitMess: ObitMess
  	ObitMess &
  	# Need to wait to start
  	python testObitMess.py
- 
+
  # Copy to where it should go
 -install: @exec_prefix@/bin
 +install: @exec_prefix@/bin ObitView ObitMess
  	@install_sh@ -s ObitView @exec_prefix@/bin/
  	@install_sh@ -s ObitMess @exec_prefix@/bin/
- 
+
 diff --git a/trunk/ObitSystem/Obit/python/makesetup.py b/trunk/ObitSystem/Obit/python/makesetup.py
 index 77c4e28..c69e716 100644
 --- a/trunk/ObitSystem/Obit/python/makesetup.py
 +++ b/trunk/ObitSystem/Obit/python/makesetup.py
-@@ -109,6 +109,6 @@ outfile.write('                              [\''+packageName+'_wrap.c\'],'+os.l
+@@ -111,6 +111,6 @@ outfile.write('                              [\''+packageName+'_wrap.c\'],'+os.l
  outfile.write('                              extra_compile_args='+str(compileArgs)+','+os.linesep)
  outfile.write('                              library_dirs='+str(libDirs)+','+os.linesep)
  outfile.write('                              libraries='+str(libs)+','+os.linesep)
@@ -523,11 +502,11 @@ index 9dcf2a1..d5812e2 100644
 @@ -40,7 +40,7 @@ SHTARGETS = libObit.dylib
  # list of object modules
  OBJECTS := $(wildcard *.o)
- 
+
 -all:  $(TARGETS)
 +all:  $(TARGETS) $(SHTARGETS)
  share: $(SHTARGETS)
- 
+
  #  build static Obit library
 diff --git a/trunk/ObitSystem/Obit/python/Makefile.in b/trunk/ObitSystem/Obit/python/Makefile.in
 index e6dd7f4..d8fae4e 100644
@@ -536,9 +515,11 @@ index e6dd7f4..d8fae4e 100644
 @@ -87,7 +87,7 @@ all: install
  $(TARGETS): setupdata.py $(MYLIBS)
  	rm -rf build
- 	python makesetup.py
--	python setup.py build install --install-lib=.
-+	ARCHFLAGS="-arch x86_64" python setup.py build install --install-lib=.
- 
+ 	python2.7 makesetup.py
+-	python2.7 setup.py build install --install-lib=.
+-	python3 setup.py build install --install-lib=.
++	ARCHFLAGS="-arch x86_64" python2.7 setup.py build install --install-lib=.
++	ARCHFLAGS="-arch x86_64" python3 setup.py build install --install-lib=.
+
  install: $(TARGETS)
  	mkdir -p build/site-packages
